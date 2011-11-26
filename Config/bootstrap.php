073f90182eeddcf696dce22710a544ca41efe18d
sam <sam.sherlock@gmail.com>
2011-11-26 00:51:57 +0000

    check if settings are already configured before setting them in bootstrap - allowing apps to differ from default plugin cfg

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -9,61 +9,71 @@
 /*
  * The model name used for the user role (typically 'Role' or 'Group')
  */
-Configure :: write('acl.aro.role.model', 'Role');
+if(!Configure::read('acl.aro.role.model'))
+    Configure :: write('acl.aro.role.model', 'Role');
 
 /*
  * The primary key of the role model
  *
  * (can be left empty if your primary key's name follows CakePHP conventions)('id')
  */
-Configure :: write('acl.aro.role.primary_key', '');
+if(!Configure::read('acl.aro.role.primary_key'))
+    Configure :: write('acl.aro.role.primary_key', '');
 
 /*
  * The foreign key's name for the roles
  *
  * (can be left empty if your foreign key's name follows CakePHP conventions)(e.g. 'role_id')
  */
-Configure :: write('acl.aro.role.foreign_key', '');
+if(!Configure::read('acl.aro.role.foreign_key'))
+    Configure :: write('acl.aro.role.foreign_key', '');
 
 /*
  * The model name used for the user (typically 'User')
  */
-Configure :: write('acl.aro.user.model', 'User');
+if(!Configure::read('acl.aro.user.model'))
+    Configure :: write('acl.aro.user.model', 'User');
 
 /*
  * The primary key of the user model
  *
  * (can be left empty if your primary key's name follows CakePHP conventions)('id')
  */
-Configure :: write('acl.aro.user.primary_key', '');
+if(!Configure::read('acl.aro.role.primary_key'))
+    Configure :: write('acl.aro.user.primary_key', '');
 
 /*
  * The name of the database field that can be used to display the role name
  */
-Configure :: write('acl.aro.role.display_field', 'name');
+if(!Configure::read('acl.aro.role.display_field'))
+    Configure :: write('acl.aro.role.display_field', 'name');
 
 /*
  * You can add here role id(s) that are always allowed to access the ACL plugin (by bypassing the ACL check)
  * (This may prevent a user from being rejected from the ACL plugin after a ACL permission update)
  */
-Configure :: write('acl.role.access_plugin_role_ids', array());
+if(!Configure::read('acl.role.access_plugin_role_ids'))
+    Configure :: write('acl.role.access_plugin_role_ids', array());
 
 /*
  * You can add here users id(s) that are always allowed to access the ACL plugin (by bypassing the ACL check)
  * (This may prevent a user from being rejected from the ACL plugin after a ACL permission update)
  */
-Configure :: write('acl.role.access_plugin_user_ids', array(1));
+if(!Configure::read('acl.role.access_plugin_user_ids'))
+    Configure :: write('acl.role.access_plugin_user_ids', array(1));
 
 /*
  * The users table field used as username in the views
  * It may be a table field or a SQL expression such as "CONCAT(User.lastname, ' ', User.firstname)" for MySQL or "User.lastname||' '||User.firstname" for PostgreSQL
  */
-Configure :: write('acl.user.display_name', "User.username");
+if(!Configure::read('acl.user.display_name'))
+    Configure :: write('acl.user.display_name', "User.username");
 
 /*
  * Indicates whether the presence of the Acl behavior in the user and role models must be verified when the ACL plugin is accessed
  */
-Configure :: write('acl.check_act_as_requester', true);
+if(!Configure::read('acl.check_act_as_requester'))
+    Configure :: write('acl.check_act_as_requester', true);
 
 /*
  * Add the ACL plugin 'Locale' folder to your application locales' folders
@@ -74,10 +84,11 @@ App::build(array('locales' => App::pluginPath('Acl') . DS . 'Locale'));
 /*
  * Indicates whether the roles permissions page must load through Ajax
  */
-Configure :: write('acl.gui.roles_permissions.ajax', true);
+if(!Configure::read('acl.gui.roles_permissions.ajax'))
+    Configure :: write('acl.gui.roles_permissions.ajax', true);
 
 /*
  * Indicates whether the users permissions page must load through Ajax
  */
-Configure :: write('acl.gui.users_permissions.ajax', true);
-?>
+if(!Configure::read('acl.gui.users_permissions.ajax'))
+    Configure :: write('acl.gui.users_permissions.ajax', true);
